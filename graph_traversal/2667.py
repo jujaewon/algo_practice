52e3b33a997d9d4cd86543bb65784e12d7370bbb
주재원 <lahmthebest@naver.com>
2024-07-19 22:27:50 +0900

    graph

---
+++ graph_traversal/2667.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(graph, a, b):
+    n = len(graph)
+    q = deque()
+    q.append((a, b))
+    graph[a][b] = 0
+    count = 1
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                q.append((nx, ny))
+                count += 1
+    return count
+
+
+N = int(input())
+graph = []
+for i in range(N):
+    graph.append(list(map(int, input())))
+
+cnt = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            cnt.append(bfs(graph, i, j))
+
+cnt.sort()
+print(len(cnt))
+for i in range(len(cnt)):
+    print(cnt[i])
