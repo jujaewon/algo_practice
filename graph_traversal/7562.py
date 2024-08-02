9ad17db6ed58234d0dfe1ea1cf68f080e88a0e61
주재원 <lahmthebest@naver.com>
2024-08-02 19:58:26 +0900

    graph

---
+++ graph_traversal/7562.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+
+T = int(sys.stdin.readline())
+
+dx = [1, 1, 2, 2, -1, -1, -2, -2]
+dy = [-2, 2, -1, 1, -2, 2, -1, 1]
+
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        x, y = q.popleft()
+
+        if x == ex and y == ey:
+            return chess[x][y]
+
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < L and 0 <= ny < L and chess[nx][ny] == 0:
+                q.append((nx, ny))
+                chess[nx][ny] = chess[x][y] + 1
+
+
+for _ in range(T):  # 테스트 케이스
+    L = int(input())  # 한 변의 길이
+    chess = [[0] * L for _ in range(L)]
+    x_start, y_start = map(int, input().split())  
+    ex, ey = map(int, input().split())  
+    print(bfs(x_start, y_start))
